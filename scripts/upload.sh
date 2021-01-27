989be7a91dac795128b3d1f4c81ae10f2d250903
Nikita Kulikov <nikita@kulikof.ru>
2021-01-27 21:11:22 +0200

    Fix auth

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -14,6 +14,7 @@ VERSION=${UPLOAD_FILE%.*}
 
 rm launcher.json
 JSON=$(jo version="$VERSION" downloadFullPath="https://minecraft.glitchless.ru/$VERSION.jar" SHA-256="$SHA256")
-echo "${JSON}" >> launcher.json
-scp "${JARPATH}" root@minecraft.glitchless.ru:/var/www/html/"${VERSION}".jar
-scp launcher.json root@minecraft.glitchless.ru:/var/www/html/launcher.json
+echo "${JSON}" >>launcher.json
+cp "${JARPATH}" "${VERSION}".jar
+#scp "${JARPATH}" root@minecraft.glitchless.ru:/var/www/html/"${VERSION}".jar
+#scp launcher.json root@minecraft.glitchless.ru:/var/www/html/launcher.json

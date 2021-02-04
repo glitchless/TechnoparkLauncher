7b3079057dfb381a495beceea84366a84d3a3956
Nikita Kulikov <nikita@kulikof.ru>
2021-02-04 09:31:22 +0200

    Change ip

--- src/main/kotlin/ru/lionzxy/tplauncher/minecraft/MinecraftContext.kt
+++ src/main/kotlin/ru/lionzxy/tplauncher/minecraft/MinecraftContext.kt
@@ -19,7 +19,7 @@ enum class MinecraftModpack(
         initialDownloadLink = "https://minecraft.glitchless.ru/initial/vanilla.zip",
         updateJsonLink = "https://minecraft.glitchless.ru/incremental/vanilla_changelog.json",
         updateHostLink = "https://minecraft.glitchless.ru/incremental/vanilla",
-        defaultServer = ServerInfo("mc1.glitchless.ru", "Glitchless Server", null, 25565),
+        defaultServer = ServerInfo("mc.glitchless.ru", "Vanilla Server", null, 25566),
         version = "1.16.5-forge-36.0.0"
     ),
     HEAVY(
@@ -27,7 +27,7 @@ enum class MinecraftModpack(
         initialDownloadLink = null,
         updateJsonLink = "https://minecraft.glitchless.ru/incremental/heavy_changelog.json",
         updateHostLink = "https://minecraft.glitchless.ru/incremental/heavy",
-        defaultServer = ServerInfo("mcbig.glitchless.ru", "Glitchless Modded Server", null, 25565),
+        defaultServer = ServerInfo("mc.glitchless.ru", "Heavy Server", null, 25567),
         version = "1.16.5-forge-36.0.13"
     );
 
